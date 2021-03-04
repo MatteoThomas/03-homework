5f2dc256c3a72dbcad1afa45ad7534428abb4267
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-03-03 18:20:09 -0700

    Second commit

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var uppers = "ABCDEFHGIJKLMNOPQRSTUVWXYZ";
+var lowers = "abcdefghijklmnopqrstuvwxyz";
+var numb = "0123456789";
+var sym = "!@#$%^&*()_-=+[]{}";
+var minlen = 8;
+var maxlen = 128;
+
+
+function inputCriteria() {
+  var criteria = prompt("Type the criteria you would like to include - Length, Character Types, or press cancel to skip.");
+  
+  if (criteria == "length") {
+  prompt("Enter a number between 8 -128")
+  }
+  if (criteria == "character types") {
+    prompt("Enter lowercase, uppercase, numeric, and/or special characters")
+ }
+  if (criteria == null) {
+    generatePassword
+  }
+}
+   
+function generatePassword() {
+  
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +37,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", inputCriteria);
+
